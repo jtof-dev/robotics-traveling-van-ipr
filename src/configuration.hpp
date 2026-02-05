a0d42c4daaa06612e1ea7bf00c33aa02c643a215
Andy Babcock <andybabcock@jtof.dev>
2026-02-04 19:14:47 -0700

    feat: create simple program to just spin the motors

--- src/configuration.hpp
+++ src/configuration.hpp
@@ -1,30 +1,7 @@
 #ifndef CONFIGURATION_HPP
 #define CONFIGURATION_HPP
 
-#define ENCODER_PIN_A 2
-#define ENCODER_PIN_B 3
 #define MOTOR_IN1 6
 #define MOTOR_IN2 7
-
-#define I2C_PORT i2c0
-#define SDA_PIN 4
-#define SCL_PIN 5
-
-#define WHEEL_DIAMETER_MM 65.0f
-#define WHEEL_CIRCUMFERENCE_MM 204.20f
-#define COUNTS_PER_REV 2080.0f
-#define MM_PER_COUNT 0.09817f
-
-#define AS5600_ADDR 0x36
-
-// defaults
-#define PENDULUM_SETPOINT -6.0f
-#define CART_KP 15.0f
-#define CART_KI 0.1f
-#define CART_KD 0.5f
-
-#define CMD_UPDATE_KP 0x01
-#define CMD_UPDATE_KI 0x02
-#define CMD_UPDATE_KD 0x03
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,10 @@
 #include "configuration.hpp"
 #include "hardware/gpio.h"
-#include "hardware/i2c.h"
 #include "hardware/pwm.h"
-#include "pico/multicore.h"
 #include "pico/stdlib.h"
-#include "pid.h"
 #include <math.h>
 #include <stdio.h>
 
-extern "C" {
-#include "dwm_pico_AS5600.h"
-}
-
-volatile long encoder_count = 0;
-
-void encoder_callback(uint gpio, uint32_t events) {
-  if (gpio == ENCODER_PIN_A) {
-    if (gpio_get(ENCODER_PIN_A) == gpio_get(ENCODER_PIN_B))
-      encoder_count--;
-    else
-      encoder_count++;
-  } else {
-    if (gpio_get(ENCODER_PIN_B) == gpio_get(ENCODER_PIN_A))
-      encoder_count++;
-    else
-      encoder_count--;
-  }
-}
 
 void set_motors(float power) {
   int pwm_val = (int)fabs(power);
@@ -45,24 +23,9 @@ void set_motors(float power) {
   }
 }
 
-void core1_entry() {
-  while (1) {
-    tight_loop_contents();
-  }
-}
-
 int main() {
   stdio_init_all();
   sleep_ms(2000);
-
-  i2c_init(I2C_PORT, 400 * 1000);
-  gpio_set_function(SDA_PIN, GPIO_FUNC_I2C);
-  gpio_set_function(SCL_PIN, GPIO_FUNC_I2C);
-  gpio_pull_up(SDA_PIN);
-  gpio_pull_up(SCL_PIN);
-
-  as5600_t pendulum_encoder = {0};
-  as5600_init(SDA_PIN, SCL_PIN, &pendulum_encoder);
 
   gpio_set_function(MOTOR_IN1, GPIO_FUNC_PWM);
   gpio_set_function(MOTOR_IN2, GPIO_FUNC_PWM);
@@ -70,55 +33,24 @@ int main() {
   pwm_set_wrap(slice, 255);
   pwm_set_enabled(slice, true);
 
-  gpio_init(ENCODER_PIN_A);
-  gpio_init(ENCODER_PIN_B);
-  gpio_pull_up(ENCODER_PIN_A);
-  gpio_pull_up(ENCODER_PIN_B);
-  gpio_set_irq_enabled_with_callback(ENCODER_PIN_A,
-                                     GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL,
-                                     true, &encoder_callback);
-  gpio_set_irq_enabled(ENCODER_PIN_B, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL,
-                       true);
-
-  float angle = 0.0f;
-  float output = 0.0f;
-  float set_point = PENDULUM_SETPOINT;
-
-  PID cartPID(&angle, &output, &set_point, CART_KP, CART_KI, CART_KD, REVERSE);
-  cartPID.SetMode(AUTOMATIC);
-  cartPID.SetOutputLimits(-255, 255);
-
-  multicore_launch_core1(core1_entry);
-
   while (true) {
-    if (multicore_fifo_rvalid()) {
-      uint32_t cmd = multicore_fifo_pop_blocking();
-      if (cmd == CMD_UPDATE_KP) {
-        uint32_t raw_val = multicore_fifo_pop_blocking();
-        float new_kp = (float)raw_val / 100.0f;
-        cartPID.SetTunings(new_kp, cartPID.GetKi(), cartPID.GetKd());
-      }
-    }
+    // Spin Forward (approx 50% speed)
+    printf("Spinning Forward\n");
+    set_motors(128.0f);
+    sleep_ms(4000);
 
-    uint16_t raw_angle = as5600_read_raw_angl(&pendulum_encoder);
-    angle = ((float)raw_angle / 4095.0f * 360.0f) - 180.0f;
+    // Stop briefly
+    set_motors(0);
+    sleep_ms(500);
 
-    if (fabs(angle - set_point) > 45.0f) {
-      set_motors(0);
-      sleep_ms(100);
-      continue;
-    }
+    // Spin Backward
+    printf("Spinning Backward\n");
+    set_motors(-128.0f);
+    sleep_ms(4000);
 
-    cartPID.Compute();
-
-    if (fabs(output) < (fabs(set_point) + 2)) {
-      output = 0;
-    }
-
-    set_motors(output);
-
-    printf("A: %.2f | O: %.1f | E: %ld\n", angle, output, encoder_count);
-    sleep_ms(10);
+    // Stop briefly
+    set_motors(0);
+    sleep_ms(500);
   }
 
   return 0;
